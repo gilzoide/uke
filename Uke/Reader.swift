4225adf39fd2d84bc6a321c99fa812f2b1edd82a
gil.reis <gil.reis@tfgco.com>
2020-01-05 17:02:55 -0300

    Read keypaths and numeric values

--- Uke/Reader.swift
+++ Uke/Reader.swift
@@ -9,7 +9,29 @@
 import UIKit
 
 class Reader {
+    static let keyPathCharacterSet = CharacterSet.letters.union(CharacterSet(charactersIn: "."))
+    static let newlineCharacterSet = CharacterSet.newlines
+    
     static func read(_ contents: String, into view: UkeView) -> Bool {
+        let scanner = Scanner(string: contents)
+        
+        while !scanner.isAtEnd {
+            readLine(scanner, into: view)
+        }
+        
         return true
+    }
+    
+    static func readLine(_ scanner: Scanner, into view: UkeView) {
+        if let keyPath = scanner.scanCharacters(from: keyPathCharacterSet),
+            let _ = scanner.scanString("="),
+            let valueString = scanner.scanUpToCharacters(from: newlineCharacterSet),
+            let value = readValue(valueString) {
+            view.setValue(value, forKeyPath: keyPath)
+        }
+    }
+    
+    static func readValue(_ valueString: String) -> Any? {
+        return Double(valueString)
     }
 }
--- UkeVisualizer/ViewController.swift
+++ UkeVisualizer/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let url = Bundle.main.url(forResource: "uke-compiled", withExtension: "txt"), let v = UkeView(url: url) {
+        if let url = Bundle.main.url(forResource: "rounded-corner", withExtension: "txt"), let v = UkeView(url: url) {
             v.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
             v.backgroundColor = UIColor.red
             view.addSubview(v)
